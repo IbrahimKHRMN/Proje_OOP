6f4a2fa4b7542820f98fcfe2e85d6e2989628ff3
IbrahimKHRMN <ibrahim.kahraman.4427@gmail.com>
2023-10-05 22:11:42 +0300

    View oluşturma ve methodlar ile görüntü oluşturma.

---
+++ Proje_OOP/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proje_OOP.Controllers
+{
+    public class DefaultController : Controller
+    {
+        void messages()
+        {
+            ViewBag.m1 = "Merhaba bu bir ASP.Net Core Projesidir.";
+            ViewBag.m2 = "Starta Hoşgeldiniz.";
+
+        }
+        public IActionResult Index()
+        {
+            messages();
+            return View();
+        }
+    }
+}
